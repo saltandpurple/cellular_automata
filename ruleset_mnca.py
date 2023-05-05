79eb0a94df97073bcc90fd1d5b46c194ab7ea0d3
saltandpurple <d0ghouse@freenet.de>
2023-05-05 18:41:52 +0200

    switched to numpy

--- ruleset_mnca.py
+++ ruleset_mnca.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cupy as cp
 from ruleset_interface import RulesetInterface
 from cupyx.scipy import signal
@@ -131,6 +132,6 @@ class RulesetMultipleNeighbourhoods(RulesetInterface):
         num_neighbours = signal.fftconvolve(state, self.neighbours[0], mode='same')
 
         # evalcondition.evalcondition(state, num_neighbours.astype(cp.int8), cp.int8(self.step))
-        evalcondition.evalcondition(cp.array([[1,0,1],[0,0,1]]).astype(cp.int8), cp.array([[40,42,17], [40,42,17]]).astype(cp.int8), cp.int8(self.step))
+        evalcondition.evalcondition(np.array([[1,0,1],[0,0,1]]).astype(np.int8), np.array([[40,42,17], [40,42,17]]).astype(np.int8), np.int8(self.step))
         self.step = 0 if self.step == 3 else self.step + 1
         return state
