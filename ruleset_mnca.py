0642e01a867232d555db27538ac5613d7351b75d
saltandpurple <d0ghouse@freenet.de>
2023-05-05 18:43:21 +0200

    full numpy

--- ruleset_mnca.py
+++ ruleset_mnca.py
@@ -131,7 +131,7 @@ class RulesetMultipleNeighbourhoods(RulesetInterface):
     def calculate_next_state(self, state):
         num_neighbours = signal.fftconvolve(state, self.neighbours[0], mode='same')
 
-        # evalcondition.evalcondition(state, num_neighbours.astype(cp.int8), cp.int8(self.step))
-        evalcondition.evalcondition(np.array([[1,0,1],[0,0,1]]).astype(np.int8), np.array([[40,42,17], [40,42,17]]).astype(np.int8), np.int8(self.step))
+        evalcondition.evalcondition(state.astype(np.int8), num_neighbours.astype(np.int8), np.int8(self.step))
+        # evalcondition.evalcondition(np.array([[1,0,1],[0,0,1]]).astype(np.int8), np.array([[40,42,17], [40,42,17]]).astype(np.int8), np.int8(self.step))
         self.step = 0 if self.step == 3 else self.step + 1
         return state
