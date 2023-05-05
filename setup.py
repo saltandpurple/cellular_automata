2403e688d8f2cf7fefed0dc149031d81c4f8e500
saltandpurple <d0ghouse@freenet.de>
2023-05-05 16:05:20 +0200

    renaming

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ on numpy.distutils for more information.
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration, get_info
 
-    config = Configuration('npufunc', parent_package, top_path)
+    config = Configuration('npufunc_directory', parent_package, top_path)
     config.add_extension('npufunc', ['eval_condition.c'])
 
     return config
