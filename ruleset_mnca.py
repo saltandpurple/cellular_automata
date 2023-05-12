ba29035dd7580e84420e518fec71e29e3add3740
saltandpurple <d0ghouse@freenet.de>
2023-05-12 16:57:43 +0200

    attempting to bridge array dimension differences in kernel

--- ruleset_mnca.py
+++ ruleset_mnca.py
@@ -9,8 +9,6 @@ class RulesetMultipleNeighbourhoods(RulesetInterface):
     def __init__(self):
         super().__init__()
 
-        self.step = cp.int8(0)
-
         self.neighbours = []
         # TODO: replace this with an import and editor functionality
         # Define the different neighbourhoods for each step
@@ -103,11 +101,12 @@ class RulesetMultipleNeighbourhoods(RulesetInterface):
         self.apply_rules_kernel = cp.RawKernel(
             r'''
             __global__
-            void apply_rules(int n, int8 *state, int8 *num_neighbours){
+            void apply_rules(short *state, short *num_neighbours){
+            
                 int index = blockIdx.x * blockDim.x + threadIdx.x;
                 int stride = blockDim.x * gridDim.x;
-                
-                for (int idx = index; idx < n; idx += stride){
+                        
+                for (int idx = index; idx < sizeof(state); idx += stride){
                     
                     // If no conditions are met, retain old state
                     
@@ -123,26 +122,26 @@ class RulesetMultipleNeighbourhoods(RulesetInterface):
          
                     // Neighbourhood 1
                     // If the cell has between 10 and 13 neighbours, it lives/spawns.
-                    if (10 <= num_neighbours[idx] && num_neighbours[idx] <= 13){
+                    if (10 <= num_neighbours[idx + 1 * sizeof(state)] && num_neighbours[idx + 1 * sizeof(state)] <= 13){
                         state[idx] = 1;
                     }
          
                     // Neighbourhood 2
                     // If the cell has between 9 and 21 neighbours, it dies.
-                    if (9 <= num_neighbours[idx] && num_neighbours[idx] <= 21){
+                    if (9 <= num_neighbours[idx + 2 * sizeof(state)] && num_neighbours[idx + 2 * sizeof(state)] <= 21){
                         state[idx] = 0;
                     }
                     
                     // Neighbourhood 3
                     // If the cell has between 78 and 89 neighbours, it dies.
                     // If the cell has more than 108 neighbours, it dies.            
-                    if ((78 <= num_neighbours[idx] && num_neighbours[idx] <= 89) || 108 < num_neighbours[idx]) {
+                    if ((78 <= num_neighbours[idx + 3 * sizeof(state)] && num_neighbours[idx + 3 * sizeof(state)] <= 89) || 108 < num_neighbours[idx + 3 * sizeof(state)]) {
                         state[idx] = 0;
                     }
                 }
             }
             ''',
-            'evaluate_condition'
+            'apply_rules'
         )
 
         # Simple code to test your rules for logical correctness on init
@@ -151,27 +150,21 @@ class RulesetMultipleNeighbourhoods(RulesetInterface):
         # step_stemp = 0
         # print(f'Result: {self.condition_kernel(state_temp, neighbours_temp, cp.int8(step_temp))}\n')
 
-
-
-
+    # TODO: rewrite this doc
     """
     Multiple Neighbourhoods rules:
     Different neighbourhoods + rules depending on which step we are at.
     Steps range from 0 to 3. 
     See rules + neighbourhood patterns above.
-    
-    Determine the number of neighbours for each cell based on the pattern for the current step. 
+    Determine the number of neighbours for each cell based on the pattern for the current neighbourhood. 
     Then apply the condition of the current step to each value of the array.
     """
     def calculate_next_state(self, state):
+        num_neighbours = cp.empty((4, cp.shape(state)[0], cp.shape(state)[1]), dtype=cp.int8)
+        for i in range(0, len(self.neighbours) - 1):
+            num_neighbours[i] = signal.fftconvolve(state, self.neighbours[i], mode='same')
 
-        num_neighbours = signal.fftconvolve(state, self.neighbours[0], mode='same')
-
-        # Old numpy-ufunc based call
-        # evalcondition.evalcondition(state.astype(cp.int8), num_neighbours.astype(cp.int8), cp.int8(self.step))
-
-        state = self.apply_rules_kernel(state.astype(cp.int8), num_neighbours.astype(cp.int8), self.step)
-        self.step = 0 if self.step == 3 else (self.step + 1)
+        self.apply_rules_kernel(state.astype(cp.int8), num_neighbours.astype(cp.int8), state)
         return state
 
 
